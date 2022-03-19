a479557a5bc043e7b54995581d72d96887cb0fa2
sidra <sidrasaleem600@yahoo.com>
2022-03-19 14:50:28 +0500

    commit Nested if

---
+++ src/FirstClass.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter data");
+        int marks = scanner.nextInt();
+        String name = scanner.next();
+        int x = 2;
+        int y = 3;
+//        if (marks >= 10 && marks <= 50) {
+//
+//            if (marks >= 33) {
+//                System.out.println("Pass");
+//            } else {
+//                System.out.println("fail");
+//            }
+//            if (marks >= 50) {
+//                System.out.println("grade c");
+//            } else {
+//                System.out.println("Not good marks");
+//            }
+//        } else {
+//            System.out.println("need to hardworking");
+//        }
+
+//
+//        if (name == "sidra") {
+//            System.out.println(name);
+//        } else {
+//            System.out.println("sid");
+//        }
+
+        if (x == 2) {
+            if (y == 3) {
+                System.out.println("x = 2 and y= 3");
+            }
+        }
+
+    }
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter Data");
+        int age;
+    }
+}
